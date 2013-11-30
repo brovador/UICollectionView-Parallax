8002800c7d087bd51f96b503fed30854a09c78be
brovador <brovador@gmail.com>
2013-11-30 23:20:31 +0100

    Initial Commit

---
+++ BRParallaxCollectionView/BRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BRAppDelegate.h
+//  BRParallaxCollectionView
+//
+//  Created by Jesús on 30/11/13.
+//  Copyright (c) 2013 Jesús. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BRParallaxCollectionView/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BRParallaxCollectionView
+//
+//  Created by Jesús on 30/11/13.
+//  Copyright (c) 2013 Jesús. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BRAppDelegate class]));
+    }
+}
